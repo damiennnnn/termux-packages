6c75339bd161e2a0edcbd0a815b32e7a35359e94
Damien Hall <damienhall04@gmail.com>
2025-10-18 23:15:42 +0100

    Run local image

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -57,7 +57,7 @@ else
 	VOLUME=$REPOROOT:$CONTAINER_HOME_DIR/termux-packages
 fi
 
-: ${TERMUX_BUILDER_IMAGE_NAME:=termux/package-builder}
+: ${TERMUX_BUILDER_IMAGE_NAME:=localhost/termux/package-builder}
 : ${CONTAINER_NAME:=termux-package-builder}
 
 USER=builder
